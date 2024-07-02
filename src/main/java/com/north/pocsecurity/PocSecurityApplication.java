66c927f8feca02497317b5b6ad15d010b8dce687
wadcharapong.men <wadcharapong.men@gmail.com>
2024-07-02 21:11:44 +0700

    first commit

---
+++ src/main/java/com/north/pocsecurity/PocSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.north.pocsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PocSecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/north/pocsecurity/PocSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.north.pocsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
